e36335780652298cf59b9679456ff460159c979e
ilhom <ilhommamatqulov276@gmail.com>
2023-01-31 15:03:59 +0500

    file10.py

--- file10.py
+++ file10.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    list1=data.split('\n')
+    k=len(list1[0])
+    for i in list1:
+        if k<len(i):
+            k=len(i)
+    return k
+f=open('txt_file/data10.txt')
+a=f.read()
+print(main(a))
 # Read data from file
