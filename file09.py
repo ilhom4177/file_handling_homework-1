70ad094cde311e5a808830973b51032e69c28533
ilhom <ilhommamatqulov276@gmail.com>
2023-01-31 14:53:53 +0500

    file09.py

--- file09.py
+++ file09.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    x=9
+    for i in range(len(data)):
+        if data[i].isdigit():
+            if x>=int(data[i]):
+                x=int(data[i])
+    return x
+f=open('txt_file/data09.txt')
+a=f.read()
+print(main(a))
 # Read data from file
