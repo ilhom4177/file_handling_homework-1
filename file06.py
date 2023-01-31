62f95c24b5bcc0a6fb935edc83d8bf8c70161987
ilhom <ilhommamatqulov276@gmail.com>
2023-01-31 14:22:50 +0500

    file06.py

--- file06.py
+++ file06.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    list1=data.split()
+    list2=[]
+    k=len(list1[0]) 
+    for i in list1:
+        list2.append(len(i))       
+    return list2
+f=open('txt_file/data06.txt')
+a=f.read()
+print(main(a))
 # Read data from file
